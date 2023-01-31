5d0bd40b0197c0c127225a0efc59216bbcb90d2f
Caio Teixeira <caioteixeira.dev@gmail>
2023-01-31 18:27:18 -0300

    Fix JSON serialization of services state to match the specification

--- src/main/java/com/outfit7/fun7/controller/ServiceStatusController.java
+++ src/main/java/com/outfit7/fun7/controller/ServiceStatusController.java
@@ -5,13 +5,17 @@ import com.outfit7.fun7.domain.serviceStatus.ServiceStatusData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/services")
 public class ServiceStatusController {
     @GetMapping
-    public ResponseEntity<ServiceStatusData> GetServiceStatus() {
+    public ResponseEntity<ServiceStatusData> GetServiceStatus(
+            @RequestParam("timezone") String timezone,
+            @RequestParam("userId") String userId,
+            @RequestParam("cc") String countryCode) {
         ServiceStatusData response = new ServiceStatusData(ServiceState.ENABLED, ServiceState.DISABLED, ServiceState.ENABLED);
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/outfit7/fun7/domain/serviceStatus/ServiceState.java
+++ src/main/java/com/outfit7/fun7/domain/serviceStatus/ServiceState.java
@@ -1,6 +1,11 @@
 package com.outfit7.fun7.domain.serviceStatus;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum ServiceState {
+    @JsonProperty("enabled")
     ENABLED,
+
+    @JsonProperty("disabled")
     DISABLED
 }
